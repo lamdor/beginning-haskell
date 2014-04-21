951a08144cbbcb87aa972a220e0cb62838e3eabd
Luke Amdor <luke.amdor@gmail.com>
2014-04-21 11:12:16 -0500

    start of chapter7

---
+++ chapters/Chapter7/MonadPlus.hs
@@ -0,0 +1,13 @@
+import Control.Monad
+
+broken1 :: Integer -> [Integer]
+broken1 n = [n-1, n+1]
+
+broken2 :: Integer -> [Integer]
+broken2 n = [1024, n+2]
+
+-- exercise 7-2. searching with MonadPlus
+
+find_ :: (a -> Bool) -> [a] -> Maybe a
+find_ p = msum . map pmaybe
+  where pmaybe a = if p a then Just a else Nothing
